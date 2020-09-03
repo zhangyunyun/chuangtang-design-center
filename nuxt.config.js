535b872477b0a56b2a0fdd78981fd9f777dc65eb
zhangyunyun <876023825@qq.com>
2020-09-03 16:41:48 +0800

    s
    s

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,11 +22,12 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/assets/css/reset.css'}
     ],
     script: [
       { src: '/rem/flexible.js', type: 'text/javascript', charset: 'utf-8'}
-    ],
+    ]
   },
   /*
   ** Global CSS
