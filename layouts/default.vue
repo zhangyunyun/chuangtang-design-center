d240aa87005ee976d0dd4f87eb7604e4a65a4868
zhangyunyun <876023825@qq.com>
2020-09-03 09:51:30 +0800

    c'chuangtang-design-center'

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!--公共头部组件-->
+
+
+    <!-- 路由视图，相当于router-view -->
+    <Nuxt />
+
+    <!--公共尾部组件-->
+
+  </div>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0
+}
+</style>
